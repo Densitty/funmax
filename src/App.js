76864b2d51d8ad91dd5341f578c5bbc6c7531f83
Densitty <dennis.brough@yandex.com>
2022-04-20 09:35:01 -0500

    adding eslint to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/react-in-jsx-scope */
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
