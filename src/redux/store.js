da4b76799ee2cc67fd91361031636c1ca743ea23
Densitty <dennis.brough@yandex.com>
2022-06-07 05:36:34 -0500

    adding some minor adjustments

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ import thunk from 'redux-thunk';
 //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__
 // );
 
-const initialState = [];
+const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
