c5970341b220e98acb32cc5edb39759c17a9b5ca
Densitty <dennis.brough@yandex.com>
2022-06-08 06:12:43 -0500

    creating the header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
-
-import './App.css';
-// import { Provider } from 'react-redux';
-// import store from './redux/store';
+import Header from './components/header/Header';
+import './App.scss';
 
 function App() {
-  return <h1>Funmax</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
